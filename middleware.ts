a69970297c85472ed3e9010cccb7140c7a51b8b2
Venkat Koushik <144405842+vkoushik15@users.noreply.github.com>
2025-03-24 20:36:40 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -8,7 +8,7 @@ export const config = {
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
     '/(api|trpc)(.*)',
-    ///dasdfasdasdfa
+   
   ], 
 }
 
