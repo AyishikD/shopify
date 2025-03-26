047084a7607f834f0139015bd4cba2bbfbed2e9f
Ayishik Das <99983449+AyishikD@users.noreply.github.com>
2025-03-27 02:18:36 +0530

    Revert "Make the payment route auth protected"

--- app/stripe/success/page.tsx
+++ app/stripe/success/page.tsx
@@ -1,41 +1,26 @@
-
-"use client";
-
 import { Button } from "@/components/ui/button";
 import { CheckCheck } from "lucide-react";
 import Link from "next/link";
-import { useAuth } from "@clerk/nextjs";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
 
-export default function StripeSuccess() {
-  const { isSignedIn } = useAuth();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (!isSignedIn) {
-      router.push("/sign-in"); // Redirect to login page if not signed in
-    }
-  }, [isSignedIn, router]);
-
-  if (!isSignedIn) return <p>Redirecting to login...</p>;
-
+export default function stripeSuccess() {
   return (
-    <div className="h-screen flex items-center justify-center">
-      <div className="text-center">
+    <div className="h-screen">
+      <div className="mt-32 md:max-w-[50vw] mx-auto">
         <CheckCheck className="text-green-600 w-16 h-16 mx-auto my-6" />
-        <h3 className="md:text-2xl text-lg text-gray-900 font-semibold">
-          Payment Successful!
-        </h3>
-        <p className="text-gray-600 my-2">
-          Thank you for your purchase. We hope you enjoy it!
-        </p>
-        <p>Have a great day!</p>
-        <Button asChild className="mt-5">
-          <Link href="/">Go Back</Link>
-        </Button>
+        <div className="text-center">
+          <h3 className="md:text-2xl text-base text-gray-900 font-semibold text-center">
+            Payment Done!
+          </h3>
+          <p className="text-gray-600 my-2">
+            Thank you for you pruchase We hope you enjoy it
+          </p>
+          <p>Have a great day!</p>
+
+          <Button asChild className="mt-5">
+            <Link href="/">GO back</Link>
+          </Button>
+        </div>
       </div>
     </div>
   );
 }
-
